2dbbfdc458f0fc6d450212f63acdaf30f0dc266a
Angel Alonso Fonseca <angel.fonseca@deliveryhero.com>
2020-06-05 13:06:12 +0200

    popping up

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import kivy
 kivy.require('1.7.2')
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 #setup graphics
@@ -16,9 +18,26 @@ from kivy.core.window import Window;
 Window.clearcolor = (0,0,0,1.)
 #Window.clearcolor = (1,0,0,1.)
 
-class ButtonGrid(AnchorLayout):
+
+Builder.load_string('''
+<SimpleButton>:
+    on_press: self.fire_popup()
+<SimplePopup>:
+    id:pop
+    size_hint: .4, .4
+    auto_dismiss: False
+    title: 'Aaaaargh!!'
+    Button:
+        text: 'Dismiss'
+        on_press: pop.dismiss()
+''')
+
+class SimplePopup(Popup):
+    pass
+
+class MainGrid(AnchorLayout):
     def __init__(self, **kwargs):
-        super(ButtonGrid, self).__init__(**kwargs)
+        super(MainGrid, self).__init__(**kwargs)
         self.anchor_x = 'center'
         self.anchor_y = 'center'
 
@@ -28,47 +47,15 @@ class ButtonGrid(AnchorLayout):
 
 
     def pressed(self, instance):
-        name = "name test"
-        last = "last test"
-        email = "email test"
-
-        print("Name:", name, "Last Name:", last, "Email:", email)
+        print("Aaaaargh!!")
+        pops=SimplePopup()
+        pops.open()
 
 
-class MainGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MainGrid, self).__init__(**kwargs)
-        self.cols = 1
-
-        self.inside_top = GridLayout()
-
-        self.inside = GridLayout()
-        self.inside.cols = 3
-
-        self.inside_bottom = GridLayout()
-
-        self.submit = Button(text="eMac Arena", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.inside.add_widget(self.submit)
-
-        self.add_widget(self.inside_top)
-        self.add_widget(self.inside)
-        self.add_widget(self.inside_bottom)
-
-
-    def pressed(self, instance):
-        name = "name test"
-        last = "last test"
-        email = "email test"
-
-        print("Name:", name, "Last Name:", last, "Email:", email)
 
 class MyApp(App):
     def build(self):
-        #this is where the root widget goes
-        #should be a canvas
-        #return MainGrid()
-        return ButtonGrid()
+        return MainGrid()
 
 if __name__ == '__main__' :
     MyApp().run()
